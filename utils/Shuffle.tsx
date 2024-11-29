ac59743581b0d32208c8fd6d3f0c620ce56d8ce5
odakris <christopher_denis@live.fr>
2024-11-29 10:02:48 +0100

    create shuffle function based on Fisher-Yates Shuffle algo

---
+++ utils/Shuffle.tsx
@@ -0,0 +1,12 @@
+import { Card } from "../types/types";
+
+// Fisher-Yates Shuffle
+// https://medium.com/@omar.rashid2/fisher-yates-shuffle-a2aa15578d2f
+export default function Shuffle(array: Card[]) {
+  for (let i = array.length - 1; i >= 0; i--) {
+    const randomIndex = Math.floor(Math.random() * (i + 1));
+    array.push(array[randomIndex]);
+    array.splice(randomIndex, 1);
+  }
+  return array;
+}
